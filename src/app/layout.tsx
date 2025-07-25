c85d46573db291852e6130ed38bb30e9d0557169
Arush Wadhawan <warush23@gmail.com>
2025-07-24 20:23:25 -0400

    feat: update layout.tsx with new content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,18 +54,21 @@ export default function RootLayout({
         <meta name="robots" content="index, follow" />
         <meta name="theme-color" content="#2563eb" />
         {/* Open Graph & Twitter tags are handled by next/metadata */}
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ER48SJBJWY"></script>
+        {/* Google Tag Manager */}
         <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-ER48SJBJWY');
-          `}
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KSCGW65B');`}
         </script>
+        {/* End Google Tag Manager */}
       </head>
       <body className="antialiased font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KSCGW65B"
+        height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
